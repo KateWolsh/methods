17d69c54d6a5c8fc6db8626584415a370810c008
Kate <katewolshanat@gmail.com>
2023-07-31 16:19:56 +0200

    fixed task

--- src/__tests__/character.test.js
+++ src/__tests__/character.test.js
@@ -1,4 +1,4 @@
-import { Character } from '../character';
+import { Character, Bowman, Swordsman, Magician, Undead, Zombie, Daemon } from '../character';
 
 describe('Validation data', () => {
     test('should create a character with valid types', () => {
@@ -81,3 +81,75 @@ describe('Validation data', () => {
 
 });
 
+describe('Bowman class', () => {
+    test('should create a Bowman character with correct properties', () => {
+      const bowman = new Bowman('Legolas');
+      expect(bowman.name).toBe('Legolas');
+      expect(bowman.type).toBe('Bowman');
+      expect(bowman.health).toBe(100);
+      expect(bowman.level).toBe(1);
+      expect(bowman.attack).toBe(25);
+      expect(bowman.defence).toBe(25);
+    });
+  });
+  
+  describe('Swordsman class', () => {
+    test('should create a Swordsman character with correct properties', () => {
+      const swordsman = new Swordsman('Arthur');
+      expect(swordsman.name).toBe('Arthur');
+      expect(swordsman.type).toBe('Swordsman');
+      expect(swordsman.health).toBe(100);
+      expect(swordsman.level).toBe(1);
+      expect(swordsman.attack).toBe(40);
+      expect(swordsman.defence).toBe(10);
+    });
+  });
+  
+  describe('Magician class', () => {
+    test('should create a Magician character with correct properties', () => {
+      const magician = new Magician('Gandalf');
+      expect(magician.name).toBe('Gandalf');
+      expect(magician.type).toBe('Magician');
+      expect(magician.health).toBe(100);
+      expect(magician.level).toBe(1);
+      expect(magician.attack).toBe(10);
+      expect(magician.defence).toBe(40);
+    });
+  });
+  
+  describe('Undead class', () => {
+    test('should create an Undead character with correct properties', () => {
+      const undead = new Undead('Sauron');
+      expect(undead.name).toBe('Sauron');
+      expect(undead.type).toBe('Undead');
+      expect(undead.health).toBe(100);
+      expect(undead.level).toBe(1);
+      expect(undead.attack).toBe(25);
+      expect(undead.defence).toBe(25);
+    });
+  });
+  
+  describe('Zombie class', () => {
+    test('should create a Zombie character with correct properties', () => {
+      const zombie = new Zombie('Zombie Guy');
+      expect(zombie.name).toBe('Zombie Guy');
+      expect(zombie.type).toBe('Zombie');
+      expect(zombie.health).toBe(100);
+      expect(zombie.level).toBe(1);
+      expect(zombie.attack).toBe(40);
+      expect(zombie.defence).toBe(10);
+    });
+  });
+  
+  describe('Daemon class', () => {
+    test('should create a Daemon character with correct properties', () => {
+      const daemon = new Daemon('Evil Spir');
+      expect(daemon.name).toBe('Evil Spir');
+      expect(daemon.type).toBe('Daemon');
+      expect(daemon.health).toBe(100);
+      expect(daemon.level).toBe(1);
+      expect(daemon.attack).toBe(10);
+      expect(daemon.defence).toBe(40);
+    });
+  });
+  
